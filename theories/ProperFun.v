077371fd211e90617d8eddc9d9ec246c278136a1
Fabricio S. Paranhos <paranhos.s.f@gmail.com>
2021-07-29 08:53:06 -0300

    Fun proper corrections.

--- theories/ProperFun.v
+++ theories/ProperFun.v
@@ -1,58 +1,59 @@
 From Nominal Require Import Perm.
 
 Section ProperPermFun.
-  Context (A B: Type) `{Perm A, Perm B}.
+  Context (A B: Type) `{Equiv A, Equiv B}.
   
-  Record proper_perm_fun : Type := ProperPermFun {
+  Record FunProper: Type := MFunProper {
     fproper :> A → B;
 (*  f_to : Perm A;
     f_from : Perm B; *)
-    fproper_spec : Proper ((≡@{A}) ⟹ (≡@{B})) fproper
+    fproper_spec: Proper ((≡@{A}) ⟹ (≡@{B})) fproper
   }.
 End ProperPermFun.
 
-Arguments ProperPermFun {_ _ _ _} _ {_}.
+Arguments MFunProper {_ _ _ _} _ {_}.
 Existing Instance fproper_spec.
 
-Notation "'λp' x .. y , t" :=
-  (@ProperPermFun _ _ _ _ (λ x, .. (@ProperPermFun _ _ _ _ (λ y, t) _) ..) _)
+Notation "'λₚ' x .. y , t" :=
+  (@MFunProper _ _ _ _ (λ x, .. (@MFunProper _ _ _ _ (λ y, t) _) ..) _)
   (at level 200, x binder, y binder, right associativity).
   
-Notation " A ⇥ B " := (proper_perm_fun A B) (at level 99, B at level 200, right associativity).
+Notation " A →ₚ B " := (FunProper A B) (at level 99, B at level 200, right associativity).
 
-Section proper_perm_fun.
-  Context `{Perm A, Perm B}.
+Section ProperFunEquiv.
+  Context `{Equiv A, Equiv B, !Equivalence (≡@{A}), !Equivalence (≡@{B})}.
   
-  #[global] Instance perm_fun_proper (f: A ⇥ B): Proper ((≡@{A}) ⟹ (≡@{B})) f.
-  Proof. apply fproper_spec. Qed.
+  (* #[global] Instance: (f: A ⇥ B): Proper ((≡@{A}) ⟹ (≡@{B})) f.
+  Proof. apply fproper_spec. Qed. *)
 
-  #[global] Instance perm_fun_proper_equiv: Equiv (A ⇥ B) := 
+  #[global] Instance fun_proper_equiv: Equiv (A →ₚ B) := 
     λ f g, ∀ (a : A), f a ≡@{B} g a.
 
-  #[global] Instance perm_fun_proper_equivalence : Equivalence perm_fun_proper_equiv.
+  #[global] Instance fun_proper_equivalence: Equivalence fun_proper_equiv.
   Proof. split; repeat intro; [reflexivity | symmetry | etransitivity]; eauto. Qed.
 
-  #[global] Instance: Proper (perm_fun_proper_equiv ⟹ (≡@{A}) ⟹ (≡@{B})) (fproper A B).
-  Proof. repeat intro; rewrite H4; auto. Qed.
+  #[global] Instance: Proper (fun_proper_equiv ⟹ (≡@{A}) ⟹ (≡@{B})) (fproper A B).
+  Proof. repeat intro; rewrite H2; auto. Qed.
+End ProperFunEquiv.
 
-  #[global,refine] Instance perm_fun_proper_act : PermAct (A ⇥ B) :=
-    λ p (f: A ⇥ B), (λp (a : A), p • f(-p • a)).
-  Proof.
-    - assumption.
-    - assumption.
-    - typeclasses eauto.
-    - repeat intro; apply gact_proper.
-      + reflexivity.
-      + apply fproper_spec; apply gact_proper; auto.  
+Section ProperFunPerm.
+    Context `{Perm A, Perm B}.
+
+    #[global,refine] Instance fun_proper_act: PermAct (A →ₚ B) :=
+        λ p (f: A →ₚ B), (λₚ (a : A), p • f(-p • a)).
+    Proof. 
+        all:try (assumption || typeclasses eauto);
+        repeat intro; rewrite H3; reflexivity. 
   Defined. 
 
-  #[global] Instance perm_fun_proper_perm: Perm (A ⇥ B).
-  Proof. split.
-    - apply perm_fun_proper_equivalence.
-    - intros ? ? EE f g EF ?; simpl; rewrite EE, EF; auto. 
-    - unfold equiv, perm_fun_proper_equiv; intros; simpl;
-        rewrite gact_id, grp_inv_neutral, gact_id; reflexivity.
-    - unfold equiv, perm_fun_proper_equiv; intros; simpl;
-        rewrite <-perm_op_inv, 2!gact_compat; reflexivity.
+  #[global] Instance FunProperPerm: Perm (A →ₚ B).
+  Proof. 
+      split.
+        - apply fun_proper_equivalence.
+        - intros ? ? EE f g EF ?; simpl; rewrite EE, EF; auto. 
+        - unfold equiv, fun_proper_equiv; intros; simpl;
+            rewrite gact_id, grp_inv_neutral, gact_id; reflexivity.
+        - unfold equiv, fun_proper_equiv; intros; simpl;
+            rewrite <-perm_op_inv, 2!gact_compat; reflexivity.
   Qed.
-End proper_perm_fun.
+End ProperFunPerm.
