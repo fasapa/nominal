5246215c948781fabfdb43c64535522978465f3f
Fabricio S. Paranhos <paranhos.s.f@gmail.com>
2021-09-01 08:51:36 -0300

    Fresh refactor

---
+++ theories/Fresh.v
@@ -0,0 +1,32 @@
+From Nominal Require Import Nominal.
+
+(* Freshness *)
+Definition fresh_e `{Nominal X} (a: name) (x: X) :=
+  ∃ (b : name), b ∉ support x ∧ ⟨a,b⟩ • x ≡@{X} x.
+
+Definition fresh_a `{Nominal X} (a: name) (x: X) :=
+  ∀ (b : name), b ∉ support x → ⟨a,b⟩ • x ≡ x.
+
+(* Infix "#" := freshness (at level 50). *)
+Infix "#ₑ" := fresh_e (at level 50).
+Infix "#ₐ" := fresh_a (at level 50).
+
+Lemma some_any `{Nominal X} (a: name) (x: X) : a #ₑ x ↔ a #ₐ x.
+Proof.
+  split.
+  - intros [b [SB HH]] c SC; destruct (decide (c = a)), (decide (c = b)); subst; auto.
+    + rewrite swap_equiv_neutral; apply gact_id.
+    + rewrite swap_expand with (b := b); try rewrite <-2!gact_compat, HH, (support_spec _ b c); auto.
+  - intros HH; exists (fresh (support x)); split.
+    + apply is_fresh.
+    + apply HH, is_fresh.
+Qed.
+
+Lemma support_fresh_e `{Nominal X} (a : name) (x: X):
+  a ∉ support x → a #ₑ x.
+Proof.
+  intros; exists a; split; [idtac | rewrite swap_equiv_neutral; apply gact_id]; auto.
+Qed.
+
+Lemma support_fresh_a `{Nominal X} (a : name) (x: X): a ∉ support x → a #ₐ x.
+Proof. intros; apply some_any, support_fresh_e; auto. Qed.
