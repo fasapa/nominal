a2cf91944266b29c880ed00c6871b9e4145a2c7b
Fabricio S. Paranhos <fabricio@nethermind.io>
2023-02-06 16:53:46 -0300

    Lambda calculus experiment

---
+++ Lambda.v
@@ -0,0 +1,162 @@
+From Nominal Require Import Nominal Fresh Alpha.
+From Nominal Require Import Instances.Name.
+
+Require Import FunInd.
+
+Inductive Term: Type :=
+| Var: Name → Term
+| App: Term → Term → Term
+| Lam: (Name * Term) → Term.
+
+Definition term_rect := fun (P : Term → Type)
+  (fvar : ∀ a : Name, P (Var a))
+  (fapp : ∀ t1: Term, P t1 → ∀ t2: Term, P t2 → P (App t1 t2))
+  (flam : ∀ a : Name, ∀ t: Term, P t → P (Lam (a,t))) =>
+  fix F (t: Term) : P t :=
+    match t as t' return (P t') with
+    | Var a => fvar a
+    | App t1 t2 => fapp t1 (F t1) t2 (F t2)
+    | Lam (a, t) => flam a t (F t)
+    end.
+
+Definition term_rec (P : Term → Set) := term_rect P.
+Definition term_ind (P : Term → Prop) := term_rect P.
+
+Fixpoint atm (t: Term) : NameSet.
+Proof.
+  apply term_rec.
+  - exact (λ a, {[ a ]}).
+  - exact (λ _ fm _ fn, fm ∪ fn).
+  - exact (λ a _ fm, {[ a ]} ∪ fm).
+  - exact t.
+Defined.
+
+Fixpoint atm1 (t: Term) : NameSet :=
+  match t with
+  | Var a => {[ a ]}
+  | App t1 t2 => (atm1 t1) ∪ (atm1 t2)
+  | Lam (a,t) => {[ a ]} ∪ (atm1 t)
+  end.
+
+Infix "=n" := Atom.dec (at level 90, no associativity).
+
+Definition subst_name (a b c: Name): Name :=
+  if a =n c then b else a.
+
+Fixpoint subst (t: Term) (a' a: Name) : Term :=
+  match t with
+  | Var c => Var (subst_name c a' a)
+  | App t1 t2 => App (subst t1 a' a) (subst t2 a' a)
+  | Lam (c,t) => Lam ((subst_name c a' a), (subst t a' a))
+  end.
+
+Inductive aeq: Term → Term → Prop :=
+| AeqVar: ∀ a, aeq (Var a) (Var a)
+| AeqApp: ∀ m n m' n', aeq m m' → aeq n n' → aeq (App m n) (App m' n')
+| AeqAbs: ∀ (a b c: Name) (m n: Term),
+    (c <> a ∧ c <> b) → (c ∉ atm1 m ∧ c ∉ atm1 n) →
+    aeq (subst m c a) (subst n c b) → aeq (Lam (a,m)) (Lam (b,n)).
+
+(* Necessario alguma relacao proper sobre os argumentos de aeq para
+  facilitar reescrita *)
+
+Lemma aeq_subst: ∀ m n a b, aeq m n → aeq (subst m a b) (subst n a b).
+Proof. Admitted.
+
+#[export] Instance subst_proper: Proper (aeq ==> eq ==> eq ==> aeq) subst.
+Proof. repeat intro; subst; apply aeq_subst; auto. Qed.
+
+#[export] Instance: Equiv Term := aeq.
+#[export] Instance: Reflexive aeq.
+Proof.
+  intro x; induction x using term_ind.
+  - constructor.
+  - constructor; auto.
+  - econstructor.
+    + (* facil, basta pegar um C diferente *) admit.
+    + (* facil, basta pegar um C diferente *) admit.
+    + apply aeq_subst; auto.
+Admitted.
+
+#[export] Instance: Symmetric aeq.
+Proof.
+  repeat intro; induction H.
+  - constructor.
+  - constructor; auto.
+  - econstructor; [destruct H; split; eauto | intuition | auto].
+Qed.
+
+#[export] Instance: Transitive aeq.
+Proof. Admitted.
+
+#[export] Instance: Equivalence aeq.
+Proof. split; typeclasses eauto. Qed.
+
+#[export] Instance: Equiv Term := aeq.
+
+Fixpoint taction (p: Perm) (t: Term): Term :=
+  match t with
+  | Var a => Var (p • a)
+  | App m n => App (taction p m) (taction p n)
+  | Lam (a,m) => Lam ((p • a), (taction p m))
+  end.
+
+#[export] Instance: PermAction Term := taction.
+
+Fixpoint fv (t: Term): NameSet :=
+  match t with
+  | Var a => {[ a ]}
+  | App m n => (fv m) ∪ (fv n)
+  | Lam (a,m) => (fv m) ∖ {[ a ]}
+  end.
+
+#[export] Instance: Support Term := fv.
+
+#[export] Instance: PermT Term.
+Proof.
+  split.
+  - typeclasses eauto.
+  - intros p q PEq m n TEq; (* rewrite PEq *)
+    unfold equiv, perm_equiv, Equiv_instance_1 in *.
+    unfold action, PermAction_instance_0 in *.
+    induction TEq; simpl.
+    + rewrite PEq. reflexivity.
+    + constructor; auto.
+    + econstructor.
+      * (* facil, basta pegar um C diferente *) admit.
+      * (* facil, basta pegar um C diferente *) admit.
+      * (* algume lema que relacione action e subst. *) admit.
+  - intro t; induction t using term_ind; unfold action, PermAction_instance_0 in *;
+    simpl.
+    + rewrite gact_id; reflexivity.
+    + constructor; [rewrite IHt1 | rewrite IHt2]; reflexivity.
+    + rewrite gact_id. econstructor.
+      * admit.
+      * admit.
+      * unfold action, PermAction_instance_0 in *; simpl; rewrite IHt; reflexivity.
+  - intros p q t; induction t using term_ind; unfold action, PermAction_instance_0 in *;
+    simpl.
+    + rewrite gact_compat; reflexivity.
+    + constructor; auto.
+    + rewrite gact_compat; econstructor.
+      * admit.
+      * admit.
+      * rewrite IHt; reflexivity.      
+Admitted.
+
+#[export] Instance: Nominal Term.
+Proof.
+    split.
+    - typeclasses eauto.
+    - intros t a b Sa Sb;
+      unfold action, PermAction_instance_0, support, Support_instance_0,
+                equiv, Equiv_instance_1 in *.
+      induction t using term_ind; simpl in *.
+      + rewrite support_spec; auto; reflexivity.
+      + constructor; [apply IHt1 | apply IHt2]; set_solver.
+      + apply not_elem_of_difference in Sa, Sb; destruct Sa, Sb.
+        * econstructor; admit. (* a ∉ fv t ∧ b ∉ fv t*)
+        * econstructor; admit. (* a ∉ fv t ∧ b = a0 *)
+        * econstructor; admit. (* a = a0 ∧ b ∉ fv t*)
+        * econstructor; admit. (* a = a0 ∧ b = a0 *)
+Admitted.
