046e2c274015819da96919f68bb3d11b80157148
Fabricio S. Paranhos <paranhos.s.f@gmail.com>
2024-04-26 22:02:39 -0300

    Freshness Theorem

--- Lambda.v
+++ Lambda.v
@@ -384,10 +384,59 @@ Admitted. *)
         * econstructor; admit. (* a = a0 ∧ b ∉ fv t*)
         * econstructor; admit. a = a0 ∧ b = a0 *)
 
-From Nominal Require Import Instances.SupportedFunctions
-  Instances.Name Instances.Prod Instances.Perm.
+Lemma action_var a b c: ⟨a,b⟩ • Var c = Var (⟨a,b⟩•c).
+Proof. unfold action; simpl; reflexivity. Qed.
 
-Definition FCB `{Nominal X, Nominal Y} (f: X →ₛ Y) :=
+Lemma action_lam a b c t: ⟨a,b⟩ • Lam c t = Lam (⟨a,b⟩•c) (⟨a,b⟩•t).
+Proof. unfold action; simpl; reflexivity. Qed.
+
+Lemma action_app a b m n: ⟨a,b⟩ • App m n = App (⟨a,b⟩•m) (⟨a,b⟩•n).
+Proof. unfold action; simpl; reflexivity. Qed.
+
+Lemma perm_var p a : p • Var a = Var (p • a).
+Proof. unfold action; simpl; reflexivity. Qed.
+
+Lemma perm_app p m n: p • App m n = App (p•m) (p•n).
+Proof. unfold action; simpl; reflexivity. Qed.
+
+Lemma perm_lam p a t: p • (Lam a t) = Lam (p•a) (p•t).
+Proof. unfold action; simpl; reflexivity. Qed.
+
+From Nominal.Instances Require Import SupportedFunctions Name Prod Perm.
+
+Section FreshnessTheorem.
+  Context `{Nominal X} (h: Name →ₛ X).
+  
+  Definition freshF : X := h (fresh (support h)).
+
+  Lemma freshness_theorem_some_any :
+    (∃ a, a # h ∧ a # (h a)) ↔ (∀ a, a # h → a # (h a)).
+  Proof.
+    split; intros HH.
+    - intros a AH; destruct HH as [b [BH1 BH2]]; destruct (decide (a = b)).
+      + subst; assumption.
+      + apply (fresh_equivariant ⟨a,b⟩) in BH2; 
+        rewrite perm_swap_right,fun_equivar,perm_swap_right,fresh_fixpoint in BH2; assumption.
+    - new c fresh h; exists c; split.
+      + apply support_fresh; assumption.
+      + apply HH, support_fresh; assumption.
+  Qed.
+
+  Theorem freshness_theorem (Haf: ∃ a, a # h ∧ a # (h a)) :
+    ∀ a, a # h → (h a) ≡ freshF.
+  Proof.
+    intros a AH; unfold freshF; set (a' := fresh _); destruct (decide (a = a')); subst; try reflexivity.
+    rewrite <-(fresh_fixpoint a a' (h a')), fun_equivar,perm_swap_right,fresh_fixpoint.
+      + reflexivity.
+      + assumption.
+      + apply fresh_support_fresh.
+      + apply fresh_fun_supp; [| apply name_neq_fresh_iff]; assumption.
+      + apply freshness_theorem_some_any; [| apply fresh_support_fresh]; assumption.
+  Qed.
+
+End FreshnessTheorem.
+
+(* Definition FCB `{Nominal X, Nominal Y} (f: X →ₛ Y) :=
   { a | a ∉ (support f) ∧ (∀ x: X, a # (f x)) }.
 
 Theorem freshness_theorem `{Nominal X} (f: Name →ₛ X): 
@@ -406,7 +455,7 @@ Proof.
     apply fresh_fun_supp; auto; apply name_neq_fresh_iff, not_eq_sym; assumption.
   - rewrite !fresh_fixpoint; try reflexivity; try assumption;
       apply fresh_fun_supp; auto; apply name_neq_fresh_iff, not_eq_sym; assumption.
-Qed.
+Qed. *)
 
 
 (* all this lemmas can be rewritten using a much more general lemma *)
